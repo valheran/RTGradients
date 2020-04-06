dbf210c65ca605303d1cc838b9c1d5d95b85ae96
valheran <valheran@gmail.com>
2020-04-06 13:23:15 +1000

    Entered description and outline in README.md

--- RTG_base.py
+++ RTG_base.py
@@ -3,9 +3,4 @@ Reactive Transport Gradients
 A tool designed to take in arrays and calculate gradients and various other things related to trying to
 use Reactive Transport Theories to quantify and navigate hydrothermal alteration systems
 
-Basic outline of process
-*Use EDS to determine reactive transport parameters and calculate Nprog (Reaction Progress Number) for the
- geochemical marker
-*Create a 3D model of the Nprog distribution
-
 """
